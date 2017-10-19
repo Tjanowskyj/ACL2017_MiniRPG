ceb357ffbed1eb66db72ab7766a42424a0227374
Louis <louis.navone7@etu.univ-lorraine.fr>
2017-10-19 13:23:32 +0200

    Correction main + ajout build.xml

--- src/Main.java
+++ src/Main.java
@@ -4,21 +4,23 @@ import java.util.Scanner;
 
 
 public class Main {
-	public static void main(String[]args){
+	public static void main(String[]args) {
 		Game g = new Game();
 		Scanner sc = new Scanner(System.in);
+		while (true) {
 			String s = sc.next();
-			if(s.equals("z")){
+			if (s.equals("z")) {
 				g.bouger("haut");
 			}
-			if(s.equals("q")){
+			if (s.equals("q")) {
 				g.bouger("gauche");
 			}
-			if(s.equals("s")){
+			if (s.equals("s")) {
 				g.bouger("bas");
 			}
-			if(s.equals("d")){
+			if (s.equals("d")) {
 				g.bouger("droite");
 			}
 		}
+	}
 }
